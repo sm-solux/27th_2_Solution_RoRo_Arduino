2857674fa751b701ce6ad944b90ed90c88ef7868
Daeun Kim <jkde7721@naver.com>
2023-02-04 14:29:43 +0900

    적외선 센서 3개 테스트 코드 구현

--- arduinoA.ino
+++ arduinoA.ino
@@ -11,18 +11,38 @@
 #define ECHO_F 2
 
 SoftwareSerial mySerial(PIN_RX, PIN_TX);
-Ultrasonic ultrasonicRight(TRIG_R, ECHO_R);
+Ultrasonic ultrasonicRight(TRIG_R, ECHO_R), ultrasonicLeft(TRIG_L, ECHO_L), ultrasonicFront(TRIG_F, ECHO_F);
 Lcd lcd(0x27, 16, 2); 
 
 void setup() {
     mySerial.begin(9600); 
     ultrasonicRight.setup(); 
+    ultrasonicLeft.setup(); 
+    ultrasonicFront.setup(); 
     lcd.setup(); 
 }
 
 void loop() {
-    int distance = ultrasonic.readDistance(); 
-    mySerial.print(distance);
-    lcd.printDistance(distance);
-    delay(1500); 
+    int distance = ultrasonicFront.readDistance(); 
+
+    if (distance < 15) {
+        int distanceRight = ultrasonicRight.readDistance();
+        int distanceLeft = ultrasonicLeft.readDistance();
+        if (distanceRight > distanceLeft) {
+            mySerial.print(distanceRight);
+            lcd.printstr("Move to Right");
+            lcd.printDistance(distanceRight);
+        }
+        else {
+            mySerial.print(distanceLeft);
+            lcd.printstr("Move to Left");
+            lcd.printDistance(distanceLeft);
+        }
+    }
+    else {
+        mySerial.print(distance);
+        lcd.printstr("Move Forward");
+        lcd.printDistance(distance);
+    }
+    delay(1500);
 }
