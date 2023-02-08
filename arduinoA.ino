f5a9738a89ea47fe6705cf30d71b0e1f28fcaa06
Daeun Kim <jkde7721@naver.com>
2023-02-08 16:09:48 +0900

    아두이노 A, B 코드 구현

--- arduinoA.ino
+++ arduinoA.ino
@@ -1,48 +1,47 @@
 #include <SoftwareSerial.h>
 #include "ultrasonic.h"
 #include "lcd.h"
-#define PIN_RX 9
-#define PIN_TX 10 
-#define TRIG_R 7
-#define ECHO_R 6
-#define TRIG_L 5
-#define ECHO_L 4
-#define TRIG_F 3
-#define ECHO_F 2
+#include "pinNumber.h"
+#include "operationCode.h"
 
 SoftwareSerial mySerial(PIN_RX, PIN_TX);
-Ultrasonic ultrasonicRight(TRIG_R, ECHO_R), ultrasonicLeft(TRIG_L, ECHO_L), ultrasonicFront(TRIG_F, ECHO_F);
+Ultrasonic ultrasonicBottom(TRIG_B, ECHO_B), ultrasonicRight(TRIG_R, ECHO_R), ultrasonicLeft(TRIG_L, ECHO_L), ultrasonicFront(TRIG_F, ECHO_F);
 Lcd lcd(0x27, 16, 2); 
 
+int distanceBottom, distanceRight, distanceLeft, distanceFront, thresholdsFront = 15, thresholdsBottom = 20;
+
 void setup() {
-    mySerial.begin(9600); 
-    ultrasonicRight.setup(); 
-    ultrasonicLeft.setup(); 
-    ultrasonicFront.setup(); 
+    mySerial.begin(9600);
+    ultrasonicRight.setup();
+    ultrasonicLeft.setup();
+    ultrasonicFront.setup();
+    ultrasonicBottom.setup();
     lcd.setup(); 
 }
 
 void loop() {
-    int distance = ultrasonicFront.readDistance(); 
+    distanceFront = ultrasonicFront.readDistance();
+    distanceBottom = ultrasonicBottom.readDistance();
 
-    if (distance < 15) {
-        int distanceRight = ultrasonicRight.readDistance();
-        int distanceLeft = ultrasonicLeft.readDistance();
+    if (distanceFront < thresholdsFront || distanceBottom > thresholdsBottom) {
+        distanceRight = ultrasonicRight.readDistance();
+        distanceLeft = ultrasonicLeft.readDistance();
         if (distanceRight > distanceLeft) {
-            mySerial.print(distanceRight);
+            mySerial.print(getOperationCode(DirectionCode::RIGHT, SpeedCode::SLOW));
             lcd.printstr("Move to Right");
             lcd.printDistance(distanceRight);
         }
         else {
-            mySerial.print(distanceLeft);
+            mySerial.print(getOperationCode(DirectionCode::LEFT, SpeedCode::SLOW));
             lcd.printstr("Move to Left");
             lcd.printDistance(distanceLeft);
         }
     }
     else {
-        mySerial.print(distance);
+        mySerial.print(getOperationCode(DirectionCode::FRONT, SpeedCode::FAST));
         lcd.printstr("Move Forward");
-        lcd.printDistance(distance);
+        lcd.printDistance(distanceFront);
     }
+
     delay(1500);
 }
--- arduinoB.ino
+++ arduinoB.ino
@@ -1,50 +1,74 @@
-// #include <AFMotor.h> 
-// #include <SoftwareSerial.h>
-// #define PIN_RX 9 //SERVO-2
-// #define PIN_TX 10 //SER1
+#include <AFMotor.h>
+#include <SoftwareSerial.h>
+#include "pinNumber.h"
+#include "operationCode.h"
 
-// AF_DCMotor motorRight(4), motorLeft(1), motor(3); 
-// SoftwareSerial mySerial(PIN_RX, PIN_TX);
-// int speed = 150;
+AF_DCMotor motorRight(1), motorLeft(2);
+SoftwareSerial mySerial(PIN_RX, PIN_TX);
 
-// void setup() {
-//     motorLeft.setSpeed(speed);
-//     motorLeft.run(RELEASE); 
-//     motorRight.setSpeed(speed);
-//     motorRight.run(RELEASE); 
-//     motor.setSpeed(speed);
-//     motor.run(RELEASE); 
+const int maxSpeed = 250, minSpeed = 150, standardSpeed = 200;
+int currentSpeed = standardSpeed, speedGap = 10;
+DirectionCode directionCode = DirectionCode::FRONT;
+SpeedCode speedCode = SpeedCode::FAST;
 
-//     Serial.begin(9600);
-//     mySerial.begin(9600);
-// }
+void handleDirection(DirectionCode directionCode);
+void handleSpeed(SpeedCode speedCode);
 
-// void loop() {
-//     long distance = 0;
+void setup() {
+    motorRight.setSpeed(currentSpeed);
+    motorLeft.setSpeed(currentSpeed);
+    motorRight.run(RELEASE);
+    motorLeft.run(RELEASE);
 
-//     if (mySerial.available()) {
-//         distance = mySerial.parseInt(); 
-//         Serial.println(distance);  
-//     }
+    Serial.begin(9600);
+    mySerial.begin(9600);
+}
 
-//     motorRight.run(FORWARD);
-//     motorLeft.run(FORWARD);
-//     motor.run(FORWARD);
+void loop() {
+    if (mySerial.available()) {
+        String operationCode = mySerial.readString();
+        Serial.println(operationCode);
+        directionCode = parseDirectionCode(operationCode);
+        speedCode = parseSpeedCode(operationCode);
+    }
 
-//     if (distance > 50) {
-//         speed = 180;
-//         motorRight.setSpeed(speed);
-//         motorLeft.setSpeed(speed);
-//         motorRight.run(FORWARD);
-//         motorLeft.run(FORWARD);
-//     }
-//     //우회전 구현 
-//     else {
-//         speed -= 10;
-//         motorRight.setSpeed(speed);
-//         motorLeft.setSpeed(speed);
-//         motorRight.run(BACKWARD);
-//         motorLeft.run(FORWARD);
-//         delay(500);
-//     }
-// }
+    handleDirection(directionCode);
+    handleSpeed(speedCode);
+    delay(500);
+}
+
+void handleDirection(DirectionCode directionCode) {
+    if (directionCode == DirectionCode::RIGHT) {
+        motorRight.run(BACKWARD);
+        motorLeft.run(FORWARD);
+    }
+    else if (directionCode == DirectionCode::LEFT) {
+        motorRight.run(FORWARD);
+        motorLeft.run(BACKWARD);
+    }
+    else {
+        motorRight.run(FORWARD);
+        motorLeft.run(FORWARD);
+    }
+}
+
+void handleSpeed(SpeedCode speedCode) {
+    if (speedCode == SpeedCode::FAST) {
+        if (currentSpeed < maxSpeed) currentSpeed += speedGap;
+    }
+    else if (speedCode == SpeedCode::SLOW) {
+        if (currentSpeed > minSpeed) currentSpeed -= speedGap;
+    }
+    else if (speedCode == SpeedCode::STOP) {
+        motorRight.setSpeed(0);
+        motorLeft.setSpeed(0);
+        motorRight.run(RELEASE);
+        motorLeft.run(RELEASE);
+    }
+    else {
+        motorRight.setSpeed(standardSpeed);
+        motorLeft.setSpeed(standardSpeed);
+        motorRight.run(FORWARD);
+        motorLeft.run(FORWARD);
+    }
+}
