b2a069e3c1b1c01d45182c9336ec0ec96ef6d045
Daeun Kim <jkde7721@naver.com>
2023-02-08 15:54:11 +0900

    핀 번호 헤더 파일 추가

---
+++ pinNumber.h
@@ -0,0 +1,11 @@
+#define PIN_RX 9 //SERVO-2
+#define PIN_TX 10 //SER1
+
+#define TRIG_B 12
+#define ECHO_B 11 
+#define TRIG_R 7
+#define ECHO_R 6
+#define TRIG_L 5
+#define ECHO_L 4
+#define TRIG_F 3
+#define ECHO_F 2
